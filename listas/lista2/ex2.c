0dbd8259c20032c078e4698640af00df7377a4d4
Samuel Krabbe <samuelkrabbe@hotmail.com>
2024-05-17 11:04:19 -0400

    some exercises from the 2nd list

---
+++ listas/lista2/ex2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    /* cria um processo-filho */
+    fork();
+    /* cria outro processo-filho */
+    fork();
+    /* e cria um outro */
+    fork();
+    return 0;
+}
